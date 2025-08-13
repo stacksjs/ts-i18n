b0e7df49452c466af913f835c3220e7e25d83364
Chris <chrisbreuer93@gmail.com>
2025-08-13 07:25:39 -0700

    chore: wip

--- packages/ts-i18n/src/types.ts
+++ packages/ts-i18n/src/types.ts
@@ -35,13 +35,6 @@ export interface BuildResult {
 // Advanced type utilities
 // =========================
 
-/** Utility to split a string by a delimiter at the type level */
-export type Split<S extends string, D extends string> =
-  S extends '' ? [] :
-    S extends `${infer T}${D}${infer U}`
-      ? [T, ...Split<U, D>]
-      : [S]
-
 /** Utility to join two path segments with a dot (avoids leading dot) */
 export type JoinPath<A extends string, B extends string> = A extends '' ? B : `${A}.${B}`
 
@@ -52,30 +45,34 @@ export type RenderableLeaf = string | number | boolean | null | undefined | ((pa
  * Produces a union of dot-separated keys for all leaf values of T.
  * Functions are considered leaves and included as keys.
  */
-export type DotPaths<T, P extends string = ''> =
-  T extends RenderableLeaf
-    ? P
-    : T extends Record<string, any>
-      ? {
-          [K in keyof T & string]:
-          T[K] extends RenderableLeaf
-            ? JoinPath<P, K>
-            : DotPaths<T[K], JoinPath<P, K>>
-        }[keyof T & string]
-      : never
+type PrevDepth = [never, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+type Dec<D extends number> = PrevDepth[D]
+
+export type DotPaths<T, P extends string = '', D extends number = 8> =
+  D extends 0
+    ? (P extends '' ? never : P)
+    : T extends RenderableLeaf
+      ? P
+      : T extends Record<string, any>
+        ? {
+            [K in keyof T & string]:
+            T[K] extends RenderableLeaf
+              ? JoinPath<P, K>
+              : DotPaths<T[K], JoinPath<P, K>, Dec<D>>
+          }[keyof T & string]
+        : never
 
 /** Resolve the value at a dot-separated path K within T */
-export type PathValue<T, K extends string> = _PathValue<T, Split<K, '.'>>
-type _PathValue<T, Ks extends readonly string[]> =
-  Ks extends []
-    ? T
-    : Ks extends [infer H, ...infer R]
+export type PathValue<T, K extends string, D extends number = 8> =
+  D extends 0
+    ? never
+    : K extends `${infer H}.${infer R}`
       ? H extends keyof T
-        ? R extends string[]
-          ? _PathValue<T[H], R>
-          : never
+        ? PathValue<T[H], R, Dec<D>>
         : never
-      : never
+      : K extends keyof T
+        ? T[K]
+        : never
 
 /** Extract the params object for a given key K (if the leaf is a function) */
 export type ParamsForLeaf<V> = V extends (...args: infer A) => any ? (A extends [] ? undefined : A[0]) : never
